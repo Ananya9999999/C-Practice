dc59e8631c67666d79c4c3bd6ecba696afe38634
Ananya Singla <imananya07@gmail.com>
2026-02-07 21:33:53 +0530

    Selection_sort

---
+++ selectionsort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void selection(int arr[], int n){
+    for (int i=0; i<n-1; i++){
+        int min_idx= i;
+        for (int j=i+1; j<n; j++){
+            if (arr[j]<arr[min_idx]){
+                min_idx=j;
+            }
+        }
+        int temp= arr[i];
+        arr[i]= arr[min_idx];
+        arr[min_idx]= temp;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i=0; i<n ; i++){
+        scanf("%d", &arr[i]);
+    }
+    selection(arr, n);
+    for (int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+}
